ba165ec1cf2f9ae368c83fc2344ae7346175a05a
sagaralmad2297 <sagaralmad1997@gmail.com>
2023-05-09 14:58:13 +0530

    fifth commit

---
+++ src/package1/Demo.java
@@ -0,0 +1,17 @@
+package package1;
+
+import org.apache.logging.log4j.*;
+
+public class Demo {
+	private static Logger log=LogManager.getLogger(Demo.class.getName());
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		log.debug("i am debuger");
+		log.error("this is error message");
+		log.debug("fuckkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkjkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkklkkkkkkkk");
+				
+
+	}
+
+}
---
+++ src/package2/beta.java
@@ -0,0 +1,18 @@
+package package2;
+
+import org.apache.logging.log4j.*;
+
+
+
+public class beta {
+
+	private static Logger log=LogManager.getLogger(beta.class.getName());
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		log.debug("i am debuger");
+		log.error("this is error message");
+		log.debug("this is new message");
+				
+}
+}
